edcecd074a42ed92fb682e24b125042ea18e946e
d1gi <digital_boy@mail.ru>
2013-07-29 02:30:57 +0700

    Роутинги для Pagerfanta

--- src/SmartCore/Bundle/BlogBundle/Controller/ArticleController.php
+++ src/SmartCore/Bundle/BlogBundle/Controller/ArticleController.php
@@ -22,30 +22,30 @@ class ArticleController extends Controller
      * @param int $num
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function pageAction(Request $request, $num = 1)
+    public function pageAction(Request $request, $page = 1)
     {
         if ($request->query->has('page')) {
-            $num = $request->query->get('page');
+            $page = $request->query->get('page');
         }
 
         $blog = $this->get('smart_blog');
 
         $articlesPerPage = $blog->getArticlesPerPage();
-        $offset          = ($num > 0) ? $articlesPerPage * ($num - 1) : 0;
+        $offset          = ($page > 0) ? $articlesPerPage * ($page - 1) : 0;
         $articles        = $blog->getArticlesByCategory(null, $articlesPerPage, $offset);
         $articlesCount   = $blog->getArticlesCountByCategory();
 
-        $pagerfanta = new Pagerfanta(new FixedAdapter($articlesCount, []));
+        $pagerfanta = new Pagerfanta(new FixedAdapter($articlesCount, $articles));
         $pagerfanta->setMaxPerPage($articlesPerPage);
 
         try {
-            $pagerfanta->setCurrentPage($num);
+            $pagerfanta->setCurrentPage($page);
         } catch (NotValidCurrentPageException $e) {
             return $this->redirect($this->generateUrl('smart_blog_index'));
         }
 
         return $this->render('SmartBlogBundle::articles.html.twig', [
-            'articles'   => $articles,
+            //'articles'   => $articles,
             'pagerfanta' => $pagerfanta,
         ]);
     }
