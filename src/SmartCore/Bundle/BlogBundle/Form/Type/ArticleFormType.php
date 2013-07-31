3d3d5069e61d5f5059e3ad8a746fedefc9159a6e
Tembich <mr.seo@mail.ru>
2013-07-31 17:50:54 +0200

    исправления

---
+++ src/SmartCore/Bundle/BlogBundle/Form/Type/ArticleFormType.php
@@ -0,0 +1,35 @@
+<?php
+namespace SmartCore\Bundle\BlogBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ArticleFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('annotation')
+            ->add('text')
+            ->add('description')
+            ->add('keywords')
+            ->add('tags')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SmartCore\Bundle\BlogBundle\Model\Article',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'article';
+    }
+}
